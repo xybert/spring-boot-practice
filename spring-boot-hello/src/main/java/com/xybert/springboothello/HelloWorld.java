88c1a5e88dce89be9478fc6eb80eee1f997af452
xybert <xybert@163.com>
2022-10-24 16:09:14 +0800

    项目重构，分为父子模块

--- spring-boot-hello/src/main/java/com/xybert/springboothello/HelloWorld.java
+++ spring-boot-hello/src/main/java/com/xybert/springboothello/HelloWorld.java
@@ -1,6 +1,6 @@
 package com.xybert.springboothello;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
