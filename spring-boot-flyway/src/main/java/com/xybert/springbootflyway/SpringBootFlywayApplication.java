66bc05e5039ea0cba472e5a1bdf6b66b47c0222f
xybert <xybert@163.com>
2022-11-04 15:50:14 +0800

    添加spring-boot-flyway模块

---
+++ spring-boot-flyway/src/main/java/com/xybert/springbootflyway/SpringBootFlywayApplication.java
@@ -0,0 +1,19 @@
+package com.xybert.springbootflyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xybert
+ * @description spring-boot-flyway 启动类
+ * @date 2022/11/04 14:27
+ */
+
+@SpringBootApplication
+public class SpringBootFlywayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFlywayApplication.class, args);
+    }
+
+}
