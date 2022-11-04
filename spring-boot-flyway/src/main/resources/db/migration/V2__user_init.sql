05fc330468deb52aefe9fe65887ca19dd5d84f4e
xybert <xybert@163.com>
2022-11-04 16:57:07 +0800

    调整字段类型

--- spring-boot-flyway/src/main/resources/db/migration/V2__user_init.sql
+++ spring-boot-flyway/src/main/resources/db/migration/V2__user_init.sql
@@ -7,10 +7,10 @@ CREATE TABLE IF NOT EXISTS `user`
 (
     `id`          bigint      NOT NULL AUTO_INCREMENT COMMENT '主键',
     `name`        varchar(64) NOT NULL COMMENT '用户名',
-    `sex`         int         NOT NULL COMMENT '性别 1-男 0-女',
+    `sex`         tinyint     NOT NULL COMMENT '性别 1-男 0-女',
     `age`         int         NOT NULL COMMENT '年龄',
     `tel`         varchar(32)          DEFAULT '' COMMENT '联系电话',
-    `email`       varchar(32)          DEFAULT '' COMMENT '邮箱',
+    `email`       varchar(64)          DEFAULT '' COMMENT '邮箱',
     `status`      tinyint     NOT NULL DEFAULT 1 COMMENT '状态 1-启用 0-禁用',
     `role`        tinyint     NOT NULL DEFAULT 3 COMMENT '角色 1-超级管理员 2-管理员 3-普通用户',
     `create_time` timestamp   NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
--- spring-boot-mybatis/src/main/resources/sql/user_init.sql
+++ spring-boot-mybatis/src/main/resources/sql/user_init.sql
@@ -5,14 +5,14 @@ USE `spring-boot-practice`;
 -- 创建表 user
 CREATE TABLE IF NOT EXISTS `user`
 (
-    `id`          bigint(20)  NOT NULL AUTO_INCREMENT COMMENT '主键',
-    `name`        varchar(50) NOT NULL COMMENT '用户名',
-    `sex`         integer(2)  NOT NULL COMMENT '性别 1-男 0-女',
-    `age`         integer(5)  NOT NULL COMMENT '年龄',
-    `tel`         varchar(20)          DEFAULT '' COMMENT '联系电话',
-    `email`       varchar(50)          DEFAULT '' COMMENT '邮箱',
-    `status`      integer(2)  NOT NULL DEFAULT 1 COMMENT '状态 1-启用 0-禁用',
-    `role`        integer(2)  NOT NULL DEFAULT 3 COMMENT '角色 1-超级管理员 2-管理员 3-普通用户',
+    `id`          bigint      NOT NULL AUTO_INCREMENT COMMENT '主键',
+    `name`        varchar(64) NOT NULL COMMENT '用户名',
+    `sex`         tinyint     NOT NULL COMMENT '性别 1-男 0-女',
+    `age`         int         NOT NULL COMMENT '年龄',
+    `tel`         varchar(32)          DEFAULT '' COMMENT '联系电话',
+    `email`       varchar(64)          DEFAULT '' COMMENT '邮箱',
+    `status`      tinyint     NOT NULL DEFAULT 1 COMMENT '状态 1-启用 0-禁用',
+    `role`        tinyint     NOT NULL DEFAULT 3 COMMENT '角色 1-超级管理员 2-管理员 3-普通用户',
     `create_time` timestamp   NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     `update_time` timestamp   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
     CONSTRAINT user_name_uindex
