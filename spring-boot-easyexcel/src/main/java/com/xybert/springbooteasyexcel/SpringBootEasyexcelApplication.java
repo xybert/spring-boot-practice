fe7a6f30da823f2f48e36c798da1128bf9171d3c
xybert <xybert@163.com>
2022-11-24 11:21:12 +0800

    添加spring-boot-easyexcel模块

---
+++ spring-boot-easyexcel/src/main/java/com/xybert/springbooteasyexcel/SpringBootEasyexcelApplication.java
@@ -0,0 +1,19 @@
+package com.xybert.springbooteasyexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xybert
+ * @description spring-boot-easyexcel 启动类
+ * @date 2022/11/24 11:16
+ */
+
+@SpringBootApplication
+public class SpringBootEasyexcelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEasyexcelApplication.class, args);
+    }
+
+}
---
+++ spring-boot-easyexcel/src/test/java/com/xybert/springbooteasyexcel/SpringBootEasyexcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xybert.springbooteasyexcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEasyexcelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
