b4e0a636ec19e5cfd567ade08d9c13530eabba5b
xybert <xybert@163.com>
2022-11-24 15:09:43 +0800

    spring-boot-easyexcel模块添加统一异常处理

---
+++ spring-boot-easyexcel/src/main/java/com/xybert/springbooteasyexcel/constant/SystemResultCode.java
@@ -0,0 +1,40 @@
+package com.xybert.springbooteasyexcel.constant;
+
+import com.xybert.springbooteasyexcel.exception.ErrorCodeBase;
+import lombok.Getter;
+
+/**
+ * @author xybert
+ * @description 状态码规约
+ * @date 2022/11/24 13:42
+ */
+
+@Getter
+public enum SystemResultCode implements ErrorCodeBase {
+
+    /**
+     * 系统异常
+     */
+    SYSTEM_NOT_HANDLER_EXCEPTION("09999", "系统内部错误", "system error"),
+    USER_NOT_EXIST("29001", "用户不存在", "user not exist"),
+    ;
+
+    /**
+     * 状态码
+     */
+    final String code;
+    /**
+     * 中文提示信息
+     */
+    final String cnMsg;
+    /**
+     * 英文提示信息
+     */
+    final String enMsg;
+
+    SystemResultCode(String code, String cnMsg, String enMsg) {
+        this.code = code;
+        this.cnMsg = cnMsg;
+        this.enMsg = enMsg;
+    }
+}
