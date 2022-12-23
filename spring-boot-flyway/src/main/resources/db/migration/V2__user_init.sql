fecf8f0f87f66cfacaf75c2b46b0e788554f4173
xybert <2676774737@qq.com>
2022-12-23 15:40:08 +0800

    调整表结构

--- spring-boot-flyway/src/main/resources/db/migration/V2__user_init.sql
+++ spring-boot-flyway/src/main/resources/db/migration/V2__user_init.sql
@@ -6,22 +6,22 @@ USE `spring-boot-practice`;
 CREATE TABLE IF NOT EXISTS `user`
 (
     `id`          BIGINT      NOT NULL AUTO_INCREMENT COMMENT '主键',
-    `account`        VARCHAR(64) NOT NULL COMMENT '用户名',
+    `account`     VARCHAR(64) NOT NULL COMMENT '用户名',
     `sex`         TINYINT COMMENT '性别 1-男 0-女',
     `age`         INT COMMENT '年龄',
     `tel`         VARCHAR(32)          DEFAULT '' COMMENT '联系电话',
     `email`       VARCHAR(64)          DEFAULT '' COMMENT '邮箱',
     `status`      TINYINT     NOT NULL DEFAULT 1 COMMENT '状态 1-启用 0-禁用',
     `create_user` BIGINT      NOT NULL DEFAULT 1 COMMENT '创建人',
-    `update_user` BIGINT      NOT NULL DEFAULT 1 COMMENT '更新人',
+    `update_user` BIGINT COMMENT '更新人',
     `create_time` DATETIME    NOT NULL DEFAULT NOW() COMMENT '创建时间',
-    `update_time` DATETIME    NOT NULL DEFAULT NOW() ON UPDATE NOW() COMMENT '修改时间',
+    `update_time` DATETIME ON UPDATE NOW() COMMENT '更新时间',
     `deleted`     TINYINT     NOT NULL DEFAULT 0 COMMENT '逻辑删除',
     CONSTRAINT account_uindex
         UNIQUE (account),
     PRIMARY KEY (`id`) USING BTREE,
-    FOREIGN KEY (`create_user`) REFERENCES user(`id`),
-    FOREIGN KEY (`update_user`) REFERENCES user(`id`)
+    FOREIGN KEY (`create_user`) REFERENCES user (`id`),
+    FOREIGN KEY (`update_user`) REFERENCES user (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
     COMMENT '用户表';
--- spring-boot-flyway/src/main/resources/db/migration/V3__role_init.sql
+++ spring-boot-flyway/src/main/resources/db/migration/V3__role_init.sql
@@ -5,15 +5,15 @@ CREATE TABLE IF NOT EXISTS `role`
     `id`          BIGINT      NOT NULL AUTO_INCREMENT COMMENT '主键',
     `name`        VARCHAR(64) NOT NULL COMMENT '角色名称',
     `create_user` BIGINT      NOT NULL DEFAULT 1 COMMENT '创建人',
-    `update_user` BIGINT      NOT NULL DEFAULT 1 COMMENT '修改人',
+    `update_user` BIGINT COMMENT '修改人',
     `create_time` DATETIME    NOT NULL DEFAULT NOW() COMMENT '创建时间',
-    `update_time` DATETIME    NOT NULL DEFAULT NOW() ON UPDATE NOW() COMMENT '修改时间',
+    `update_time` DATETIME ON UPDATE NOW() COMMENT '更新时间',
     `deleted`     TINYINT     NOT NULL DEFAULT 0 COMMENT '逻辑删除',
     CONSTRAINT role_name_uindex
         UNIQUE (name),
     PRIMARY KEY (`id`) USING BTREE,
-    FOREIGN KEY (`create_user`) REFERENCES user(`id`),
-    FOREIGN KEY (`update_user`) REFERENCES user(`id`)
+    FOREIGN KEY (`create_user`) REFERENCES user (`id`),
+    FOREIGN KEY (`update_user`) REFERENCES user (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
     COMMENT '角色表';
