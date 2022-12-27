895fd77a114265314aea83d8ddda681e0a6fdb98
xybert <2676774737@qq.com>
2022-12-27 17:56:25 +0800

    新增spring-boot-jpa模块

---
+++ spring-boot-jpa/src/main/java/com/xybert/springbootjpa/SpringBootJpaApplication.java
@@ -0,0 +1,19 @@
+package com.xybert.springbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xybert
+ * @description spring-boot-jpa启动类
+ * @date 2022/12/27 17:44
+ */
+
+@SpringBootApplication
+public class SpringBootJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJpaApplication.class, args);
+    }
+
+}
