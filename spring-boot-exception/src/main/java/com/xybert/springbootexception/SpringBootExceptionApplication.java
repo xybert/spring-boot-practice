22ba100615e0e540b425755108895571dee043b4
xybert <2676774737@qq.com>
2022-12-30 17:58:35 +0800

    添加统一异常处理模块

---
+++ spring-boot-exception/src/main/java/com/xybert/springbootexception/SpringBootExceptionApplication.java
@@ -0,0 +1,19 @@
+package com.xybert.springbootexception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xybert
+ * @description spring-boot-exception启动类
+ * @date 2022/12/30 10:15
+ */
+
+@SpringBootApplication
+public class SpringBootExceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExceptionApplication.class, args);
+	}
+
+}
